e9b41f5f724ac91a62c85816beb60e2ea60cbeab
Vic Segers <vic.segers10@gmail.com>
2020-12-02 16:12:31 +0100

    Day 1

--- day_1/answer.py
+++ day_1/answer.py
@@ -1,15 +1,22 @@
 #!/usr/local/bin/python3
 
-
-def factor_where_sum_is(target):
+def part_1(values, target):
     for value in values:
         for value2 in values:
             if value + value2 == 2020:
                 return value * value2
+
+def part_2(values, target):
+    for value in values:
+        for value2 in values:
+            for value3 in values:
+                if value + value2 + value3 == 2020:
+                    return value * value2 * value3
 
 
 if __name__ == "__main__":
     with open("input.txt") as f:
         values = [int(line[:-1]) for line in f.readlines()]
 
-    print(factor_where_sum_is(2020))
+    print(f"Part 1: {part_1(values, 2020)}")
+    print(f"Part 2: {part_2(values, 2020)}")
